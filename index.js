ffe8fa0d49158be8364285fdaf1078a3acfe2b32
herve <hdjomo1@gmail.com>
2023-11-02 11:06:08 +0100

    first work

--- index.js
+++ index.js
@@ -4,21 +4,25 @@
 
 //simple
 // je veux qu'un compteur incrémente a chaque fois qu'on click sur sign-in
-let i=0;
-    let counter= document.getElementById("submit").addEventListener("click",function insert(){ i += 1
-         innerHtml = `compteur = ${counter}`})
+const count = document.getElementById('counter');
+let i = 0;
+
+document.getElementById("submit").addEventListener("click", function() {
+    i += 1;
+    count.textContent = i;
+    // Add this code to your JavaScript file (index.js)
+
+
+});
+
     
 
     
 // je veux savoir qui a essayer de se connecter [email, password]
-let pass = document.getElementById(password)
-let mail = document.getElementById(email)
-for(i=0;i<100;i++)pass + mail
+
 //intermediate
 // je veux le nombre d'essaie ok et not ok
-if(mail == email && pass == password){y += 1}else{
-    p+=1
-}
+
 // je veux qu'au 5ieme essaie ok, on previenne l'utilisateur qu'il se rediriger 
 
 //hard
