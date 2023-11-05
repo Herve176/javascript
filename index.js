d0353a17d6a8566fedd3d88c345a3c4a9a3357e4
herve <hdjomo1@gmail.com>
2023-11-05 16:02:47 +0100

    started with the second excercise

--- index.js
+++ index.js
@@ -5,24 +5,29 @@
 //simple
 // je veux qu'un compteur incrémente a chaque fois qu'on click sur sign-in
 const count = document.getElementById("counter")
+const show = document.getElementById("show")
 let i = 0;
-
+let email = Document.getElementById("email");
+let password = Document.getElementById("password");
 document.getElementById("submit").addEventListener("click", function(event) {
     
     i += 1;
     count.innerHTML = i
+    show.innerHTML=email; password;
     event.preventDefault();
     // Add this code to your JavaScript file (index.js)
+   });
 
 
 
-    
-})
 
     
 
     
 // je veux savoir qui a essayer de se connecter [email, password]
+
+
+
 
 //intermediate
 // je veux le nombre d'essaie ok et not ok
