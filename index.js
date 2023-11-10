51d651b977a8b5b1c303e374726ce0e85523a372
herve <hdjomo1@gmail.com>
2023-11-10 16:10:37 +0100

    worked on alert

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let i = 0
 let j=0
 let emails='rode@rode.com'
 let passwords = 'password123'
+
 document.getElementById("submit").addEventListener("click", function listen(event) {
     
     event.preventDefault();
@@ -21,9 +22,9 @@ document.getElementById("submit").addEventListener("click", function listen(even
     i += 1;
     countpos.innerHTML = "positive attempt:" + i
     showme.append("correct connected:"+ email.value + password.value)
-    if(i == 5){function Redirect(){window.location = "C:\Users\hdjom\OneDrive - stud.thga.de\Dokumente\web dev mit rodrigue\javascript\index.html"}
+    if(i == 5){function Redirect(){window.location = "www.google.com"}
     window.alert("Please go back to start site.you will be redirected in 10s")
-    setTimeout(function(){Redirect()},1000) 
+    setTimeout(function(){Redirect()},10000) 
     }
     }else{
     j += 1;
